551d1bb30ea1c71f6ab83fc56b1f9bd537fed1a0
Oliwia Głuszek <oliwiawnetrzak@gmail.com>
2022-11-09 16:32:30 +0100

    Implement item model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,12 @@
+class ItemModel {
+  ItemModel({
+    required this.title,
+    required this.id,
+    required this.imageURL,
+    required this.relaseDate,
+  });
+  final String title;
+  final String id;
+  final String imageURL;
+  final DateTime relaseDate;
+}
